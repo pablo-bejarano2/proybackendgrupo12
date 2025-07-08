7de58fd422b54d89febbf93536b197a55188e3f4
marcosAchambi <44516075@fi.unju.edu.ar>
2025-07-08 10:33:23 -0300

    Update path for codigos_postales.json in controller

--- trabajo_final_backend/controllers/codigosPostales.controlller.js
+++ trabajo_final_backend/controllers/codigosPostales.controlller.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const codigos = JSON.parse(
-  fs.readFileSync(path.join(__dirname, '../data/codigos_postales.json'), 'utf8')
+  fs.readFileSync(path.join(__dirname, '../codigos_postales.json'), 'utf8')
 );
 
 const buscarPorCP = (req, res) => {
